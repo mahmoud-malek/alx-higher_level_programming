5c20fe6fc5d848794520b41436b4b132833517e0
mahmoud malek <information.mahmoud@gmail.com>
2023-10-02 22:53:51 +0300

    fix case where null

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,9 +11,6 @@ int check_cycle(listint_t *list)
 	listint_t *fast = list;
 	listint_t *slow = list;
 
-	if (!list)
-		return (-1);
-
 	while (fast != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
