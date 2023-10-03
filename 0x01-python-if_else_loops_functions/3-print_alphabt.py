8b0a18ef8a0b90c1c6116259b81bd30018f518b6
mahmoud <information.mahmoud@gmail.com>
2023-10-03 23:31:36 +0300

    add scripts

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Hello :) I'm Mahmoud malek
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""This program Prints the numbers from 0 to 98 in decimal and hexadecimal"""
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
