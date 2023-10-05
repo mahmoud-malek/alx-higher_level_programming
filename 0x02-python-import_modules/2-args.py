eb8c3cedac36f976ebbb8a4de5281323d227d0d6
mahmoud <information.mahmoud@gmail.com>
2023-10-05 19:39:09 +0300

    push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments".format(len(sys.argv) - 1, ))
+        print("{} arguments:".format(len(sys.argv) - 1, ))
         for i in range(len(sys.argv) - 1):
             print("{}: {}".format(i + 1, sys.argv[i+1]))
