58253df5c0c1a9051fb4200353c736fe7c0a004e
mahmoud <information.mahmoud@gmail.com>
2023-10-05 20:01:43 +0300

    infinite add

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" This is a program to add all its arguments 
+	Made by: mahmoud malek
+"""
+if __name__ == "__main__":
+	
+	import sys
+	total = 0
+	for i in sys.argv[1:]:
+		total += int(i)
+	print(total)
