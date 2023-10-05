07aa949e17f4aef84afb14ca2f6616812bf2933e
mahmoud <information.mahmoud@gmail.com>
2023-10-05 19:10:34 +0300

    fix improting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-# program to add to number using add_0 function
-from add_0 import add
-a = 1
-b = 2
 if __name__ == "__main__":
+
+    # program to add to number using add_0 function
+    from add_0 import add
+
+    a = 1
+    b = 2
     print("{} + {} = {}".format(a, b,  add(a, b)))
