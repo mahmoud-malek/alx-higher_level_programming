31a4abaccfd808a7a2295c98bfaf61e6472b6255
mahmoud <information.mahmoud@gmail.com>
2023-10-25 02:24:11 +0300

    yes

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,15 +10,7 @@ class Square:
 
         Args:
             size (int): The size of the new square. Defaults to 0.
-
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property
