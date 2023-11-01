43640701bbc3be420b1f473dbcf26aad966b31bf
mahmoud <information.mahmoud@gmail.com>
2023-11-01 15:47:53 +0200

    ok

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -12,27 +12,20 @@ Example:
 
 
 def matrix_mul(m_a, m_b):
-    """
-    Function to multiply two matrices.
+    """Multiply two matrices.
 
     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
-
-    Returns:
-        list of lists of ints/floats: A new matrix representing
-                the multiplication of m_a by m_b.
-
     Raises:
-        TypeError: If either m_a or m_b is not a list of lists of
-                 integers/floats, if either m_a or m_b is empty,
-                  or if either m_a or m_b has different-sized rows.
+        TypeError: If either m_a or m_b is not a list of lists of ints/floats.
+        TypeError: If either m_a or m_b is empty.
+        TypeError: If either m_a or m_b has different-sized rows.
         ValueError: If m_a and m_b cannot be multiplied.
-
-    Example:
-        >>> matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]])
-        [[7, 10], [15, 22]]
+    Returns:
+        A new matrix representing the multiplication of m_a by m_b.
     """
+
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
@@ -56,9 +49,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must be of the same size")
+        raise TypeError("each row of m_a must should be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must be of the same size")
+        raise TypeError("each row of m_b must should be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
