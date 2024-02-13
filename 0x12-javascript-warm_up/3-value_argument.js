0a1953e0297df469137de941043f7ebc2d22808f
mahmoud malek <information.mahmoud@gmail.com>
2024-02-13 02:53:31 +0200

    fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-const len = process.argv.length;
-
-if (len === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
