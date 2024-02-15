f9d32aa78bc6ca109c3cfcccf17b7e4622307948
mahmoud malek <information.mahmoud@gmail.com>
2024-02-16 01:48:43 +0200

    filtering db rows using python

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" this module filters states by user input """
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    if (len(argv) != 4):
+        print("Usage: {} username password database_name".format(argv[0]))
+        exit(1)
+
+    username = argv[1]
+    password = argv[2]
+    dbName = argv[3]
+
+    db = MySQLdb.connect('localhost', username, password, dbName, 3306)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
