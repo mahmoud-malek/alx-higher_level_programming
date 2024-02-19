57c3ad1484fabe4d5b815bd5e1243a8c09bcd52b
mahmoud malek <information.mahmoud@gmail.com>
2024-02-19 04:17:59 +0200

    try

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -19,8 +19,12 @@ if __name__ == "__main__":
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        session.add(State(name="California", cities=[
-                    City(name="San Francisco")]))
+        new_state = State(name="California")
+        new_city = City(name="San Francisco", state=new_state)
+        new_state.cities.append(new_city)
+
+        session.add(new_state)
+        session.add(new_city)
         session.commit()
         session.close()
     else:
