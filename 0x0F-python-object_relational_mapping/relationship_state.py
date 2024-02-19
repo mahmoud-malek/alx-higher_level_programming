2cb17e73ebb020d7a6cf40864ecf70b3c4d31fae
mahmoud malek <information.mahmoud@gmail.com>
2024-02-19 04:22:31 +0200

    ok

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,11 +2,12 @@
 """ a python file that contains the class definition
  of a State and an instance Base = declarative_base(): """
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-Base = declarative_base()
+data = MetaData()
+Base = declarative_base(metadata=data)
 
 
 class State(Base):
