00876aed10ce0d2634d699b548fdd1a719de6ae7
mahmoud malek <information.mahmoud@gmail.com>
2024-03-01 07:32:45 +0200

    fix

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""a Python script that takes in a URL and an email address,
+ sends a POST request to the passed URL with the email
+ as a parameter, and finally displays the body of the response.
 """
-A Python script that takes in a URL and an email address, sends a POST request
-to the passed URL with the email as a parameter, and finally displays the body
-of the response.
-"""
-
-import requests
 import sys
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
 
-    response = requests.post(url, data={'email': email})
-    print(response.text)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
