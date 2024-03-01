abd6c93dcd9eaa97818d5878d99be9323c29b6a1
mahmoud malek <information.mahmoud@gmail.com>
2024-03-01 07:19:48 +0200

    finish

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ a Python script that takes in a URL,
- sends a request to the URL and displays the body of the response (decoded in utf-8).
+ sends a request to the URL and displays the
+  body of the response (decoded in utf-8).
 """
 
 
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A a Python script that takes in a letter and sends
- a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+ a POST request to http://0.0.0.0:5000/search_user
+  with the letter as a parameter.
 """
 import sys
 import requests
