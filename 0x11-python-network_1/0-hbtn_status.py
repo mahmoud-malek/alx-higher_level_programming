deaaa42aeb80a36f17b1c99e69242b6f2741fc3d
mahmoud malek <information.mahmoud@gmail.com>
2024-03-01 06:42:53 +0200

    ok

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status
-using the urllib package and displays the body of the response.
+This script fetches the URL https://alx-intranet.hbtn.io/status using the urllib package.
+The body of the response is displayed with a tabulation before each line.
 """
 
 import urllib.request
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
 
+def fetch_url():
+    """
+    Fetches the URL and prints the response body.
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
-        body = response.read()
-        utf8_content = body.decode('utf-8')
+        html = response.read()
+        lines = html.decode().split('\n')
+        for line in lines:
+            print('\t-', line)
 
-        print("Body response:")
-        print("\t- type:", type(body))
-        print("\t- content:", body)
-        print("\t- utf8 content:", utf8_content)
+
+if __name__ == "__main__":
+    fetch_url()
