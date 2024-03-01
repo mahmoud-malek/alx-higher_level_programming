179ce82e8e1df643ef945f6c2d227521ea0fb248
mahmoud malek <information.mahmoud@gmail.com>
2024-03-01 06:47:15 +0200

    try

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         content = res.read()
         decoded_content = content.decode('utf-8')
 
-        print("Response Body:")
-        print("\t- Data type: {}".format(type(content)))
-        print("\t- Raw content: {}".format(content))
-        print("\t- Decoded content: {}".format(decoded_content))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
